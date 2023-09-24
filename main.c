72023c5e436fbe870377c7af577bcca767e889c1
Doctor Faust <kaflanfromk@gmail.com>
2023-09-25 01:10:31 +0300

    Third step of The Pizdorez Adventure

--- main.c
+++ main.c
@@ -2,50 +2,51 @@
 
 //don't mess up [rows][columns]
 
-#define WIDTH 80
+//below two lines are like a resolution of the game screen
+#define WIDTH 120
+#define HEIGHT 40
 
-void draw_scene(char scene[][WIDTH], int height);
-void build_top(char scene[][WIDTH], int height);
-void build_left(char scene[][WIDTH], int height);
-void build_right(char scene[][WIDTH], int height);
-void build_bottom(char scene[][WIDTH], int height);
-void print(char scene[][WIDTH], int height);
-void spacefy(char scene[][WIDTH], int height);
+void draw_scene(char scene[][WIDTH]);
+void build_top(char scene[][WIDTH]);
+void build_left(char scene[][WIDTH]);
+void build_right(char scene[][WIDTH]);
+void build_bottom(char scene[][WIDTH]);
+void print(char scene[][WIDTH]);
+void spacefy(char scene[][WIDTH]);
 
 int main() {
-	int height = 30;
-	char scene_2d[height][WIDTH];
+	char scene_2d[HEIGHT][WIDTH];
 
-	spacefy(scene_2d, height);
-	draw_scene(scene_2d, height);
+	spacefy(scene_2d);
+	draw_scene(scene_2d);
 }
 
-void draw_scene(char scene[][WIDTH], int height) {
-	build_top(scene, height);
-	build_left(scene, height);
-	build_right(scene, height);
-	build_bottom(scene, height);
-	print(scene, height);
+void draw_scene(char scene[][WIDTH]) {
+	build_top(scene);
+	build_left(scene);
+	build_right(scene);
+	build_bottom(scene);
+	print(scene);
 }
 
-void build_top(char scene[][WIDTH], int height) {
+void build_top(char scene[][WIDTH]) {
 	for(int i = 0; i < WIDTH; scene[0][i++] = '#');
 }
 
-void build_left(char scene[][WIDTH], int height) {
-	//TODO
+void build_left(char scene[][WIDTH]) {
+	for(int i = 0; i < HEIGHT; scene[i++][0] = '#');
 }
 
-void build_right(char scene[][WIDTH], int height) {
-	//TODO
+void build_right(char scene[][WIDTH]) {
+	for(int i = 0; i < HEIGHT; scene[i++][WIDTH - 1] = '#');
 }
 
-void build_bottom(char scene[][WIDTH], int height) {
-	//TODO
+void build_bottom(char scene[][WIDTH]) {
+	for(int i = 0; i < WIDTH; scene[HEIGHT - 1][i++] = '#');
 }
 
-void print(char scene[][WIDTH], int height) {
-	for(int i = 0; i < height; i++) {
+void print(char scene[][WIDTH]) {
+	for(int i = 0; i < HEIGHT; i++) {
 		for(int j = 0; j < WIDTH; j++) {
 			printf("%c", scene[i][j]);
 		}
@@ -53,8 +54,8 @@ void print(char scene[][WIDTH], int height) {
 	}
 }
 
-void spacefy(char scene[][WIDTH], int height) {
-	for(int i = 0; i < height; i++) {
+void spacefy(char scene[][WIDTH]) {
+	for(int i = 0; i < HEIGHT; i++) {
 		for(int j = 0; j < WIDTH; j++) {
 			scene[i][j] = ' ';
 		}
